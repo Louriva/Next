f021dbfde0f13bea5c41294bdb236ca7760dcd92
louriva <louriva.fragoso@gmail.com>
2019-09-27 04:30:38 -0300

    swagger correçao

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb+srv://teste:teste@cluster0-gfeet.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/Next', { useNewUrlParser:true});
